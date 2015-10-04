080d85d9f2700ded05e32ede622646a06a4e2ebc
Lachezar Lalov <luchoazz@abv.bg>
2015-10-04 12:25:10 +0300

    minor updates

--- #2_Web-Development-Basics/cms-mvc-project/views/home/index.php
+++ #2_Web-Development-Basics/cms-mvc-project/views/home/index.php
@@ -1,4 +1,5 @@
- <h1>Welcome home index view!</h1>
+ <h1>Welcome!</h1>
+ <h3>More content will be added soon.</h3>
  <?php if (! $this->isLoggedIn) : ?>
     <a href="/account/login">Login</a>
     <a href="/account/register">Register</a>
