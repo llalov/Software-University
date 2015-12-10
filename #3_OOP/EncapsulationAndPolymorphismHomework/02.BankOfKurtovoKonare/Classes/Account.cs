f1500528c5afac1fdc888acf9936f5e077572c7d
Lachezar Lalov <luchoazz@abv.bg>
2015-12-10 13:19:20 +0200

    Added Encapsulation and polymorphism homework

---
+++ #3_OOP/EncapsulationAndPolymorphismHomework/02.BankOfKurtovoKonare/Classes/Account.cs
@@ -0,0 +1,33 @@
+﻿using P2_BankOfKurtovoKonare.Interfaces;
+
+namespace P2_BankOfKurtovoKonare.Classes
+{
+    abstract class Account : IInterest
+    {
+        //private Customer customer;
+        //private decimal balance;
+        //private decimal interestRate;
+
+        public Customer User { get; set; }
+        public decimal Balance { get; set; }
+        public decimal InterestRate { get; set; }
+
+        protected Account(Customer user, decimal balance, decimal interestRate)
+        {
+            this.User = user;
+            this.Balance = balance;
+            this.InterestRate = interestRate;
+        }
+        public abstract decimal CalculateInterest(decimal money, int months);
+
+        public void DepositMoney(decimal moneyAmount)
+        {
+            this.Balance += moneyAmount;
+        }
+        public override string ToString()
+        {
+            return string.Format("Account type: {0}\n{1}Balance: {2:C2}\nInterest rate: {3}",
+                this.GetType().Name, this.User, this.Balance, this.InterestRate);
+        }
+    }
+}
---
+++ #3_OOP/EncapsulationAndPolymorphismHomework/02.BankOfKurtovoKonare/Classes/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace P2_BankOfKurtovoKonare.Classes
+{
+    abstract class Customer
+    {
+        //private string address;
+        //private string phoneNumber;
+        public abstract string GetName();
+        public override string ToString()
+        {
+            return string.Format("Customer type: {0}\n",this.GetType().Name);
+        }
+    }
+}
